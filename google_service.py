e3414ec300381bb266e87a2993448f5abe43deed
paradox7077 <offfffffff7077@gmail.com>
2026-07-08 14:32:23 +0700

    Update google_service.py

--- google_service.py
+++ google_service.py
@@ -1,19 +1,24 @@
-import json
-import streamlit as st
 from google.oauth2 import service_account
 from googleapiclient.discovery import build
+
+SERVICE_ACCOUNT_FILE = "/etc/secrets/service_account.json"
 
 SCOPES = [
     "https://www.googleapis.com/auth/drive",
     "https://www.googleapis.com/auth/spreadsheets",
 ]
 
+
 def get_credentials():
-    info = json.loads(st.secrets["GOOGLE_SERVICE_ACCOUNT_JSON"])
-    return service_account.Credentials.from_service_account_info(info, scopes=SCOPES)
+    return service_account.Credentials.from_service_account_file(
+        SERVICE_ACCOUNT_FILE,
+        scopes=SCOPES
+    )
+
 
 def get_drive_service():
     return build("drive", "v3", credentials=get_credentials())
+
 
 def get_sheets_service():
     return build("sheets", "v4", credentials=get_credentials())
